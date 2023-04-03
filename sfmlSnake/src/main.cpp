1787dfa585f7838a439755f27b3d87564c01aea1
Ömer Can Karadağ <krdg.omercan@hotmail.com>
2023-04-03 23:47:12 +0300

    v1

--- sfmlSnake/src/main.cpp
+++ sfmlSnake/src/main.cpp
@@ -1,10 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Snake Game");
+    sf::RectangleShape snakeBodyPiece;
+    snakeBodyPiece.setFillColor(sf::Color::Green);
+    snakeBodyPiece.setSize(sf::Vector2f(20.f, 20.f));
+    std::vector<sf::RectangleShape> snake(3, snakeBodyPiece);
+    int headDirection = 1;// 1-North 2-East 3-South 4-west
+    std::cout << sizeof(snake);
 
     while (window.isOpen())
     {
@@ -15,9 +21,23 @@ int main()
                 window.close();
         }
 
+        switch (headDirection)
+            case 1:
+                for (size_t i; i < snake.size(); i++) {
+
+                }
+        
+
+
         window.clear();
-        window.draw(shape);
+
+
+        window.draw(snakeBodyPiece);
+        
+
+
         window.display();
+
     }
 
     return 0;
